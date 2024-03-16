5217d8a392530f08c75d1f4a7ccc845eebbec71c
alegonren <138701785+alegonren@users.noreply.github.com>
2024-03-16 01:05:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarDetalle(opcionSeleccionada) {
+    if (opcionSeleccionada) {
+        window.location.href = opcionSeleccionada + ".html";
+	window.location.replace(opcionSeleccionada + ".html");
+    }
+}
